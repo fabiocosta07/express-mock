14c096aefbb3ef36e5630acd87d7c516faa369a8
Fabio Apolonio <fabio.apolonio@csaa.com>
2022-02-04 12:52:55 -0700

    mock

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express')
+const cors  = require('cors')
+var bodyParser = require('body-parser')
 const app = express()
+app.use(cors())
+
 const port = 9000
 
 app.get('/effective-date', (req, res) => {
+  res.send({date: new Date})
+})
+
+app.put('/effective-date', (req, res) => {
+  console.log(req.body)
   res.send({date: new Date})
 })
 
